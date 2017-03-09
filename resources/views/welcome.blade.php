a4349f8f460612ea681fd0696c9b0fce00862949
YISECA <yiseca@gmail.com>
2017-03-09 08:33:46 -0500

    INICIO

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,295 @@
+@extends('master')                              
+
+@section('content')         
+
+<link rel="stylesheet" type="text/css" href="public/Css/form.css">
+
+<link rel="stylesheet" type="text/css" href="public/Css/slider_styles.css">
+
+<section id="page1">
+
+    <div class="panel panel-default">
+
+        <div class="panel-heading">Inicio</div>
+
+        <div class="panel-body">
+
+            <div class="freebirdFormviewerViewFormContent ">
+
+                <div class="freebirdFormviewerViewHeaderHeader">
+
+                    <div class="freebirdFormviewerViewHeaderTitleRow">
+
+                        <div class="freebirdFormviewerViewHeaderTitle" dir="auto" role="heading" aria-level="1">PARQUE ECOLÓGICO CANTARRANA - 05 de marzo de 2017</div>
+                        <div class="freebirdFormviewerViewHeaderTitle" dir="auto" role="heading" aria-level="1">“CAMINATA ESPECIAL PARA SU MASCOTA”</div>
+
+                    </div>
+<br>
+                    <div class="stage">
+
+                        <div id="SLDR-ONE" class="sldr">
+
+                            <ul class="wrp animate">
+
+                                <li class="elmnt-one">                                 
+
+                                        <div class="wrap"><img src="public/Img/canta.jpg"  height="400">
+
+                                        </div>                                   
+                                </li>
+
+                               <li class="elmnt-two">
+                                    
+                                        <div class="wrap"><img src="public/Img/canta1.jpg"  height="400">
+
+                                        </div>                                 
+                                </li>
+
+                                 <li class="elmnt-three">
+                                    
+                                        <div class="wrap"><img src="public/Img/canta2.jpg"  height="400">
+
+                                        </div>                                 
+                                </li>
+
+                            </ul>
+
+                        </div>
+
+                        <div class="clear"></div>
+
+                        <ul class="selectors">
+
+                            <li class="focalPoint"><a href="">•</a></li>
+
+                            <li><a href="">•</a></li>
+
+                            <li><a href="">•</a></li>
+                         
+                        </ul>
+
+                    </div>
+
+                    <script src="public/Js/jquery.sldr.js"></script>
+
+                    <div class="freebirdFormviewerViewHeaderDescription" dir="auto">
+
+                        <h2>Ubicación</h2>
+
+                        <p><font size="3">El parque ecológico Cantarrana se encuentra en la localidad No. 5 de Usme. Su ingreso está ubicado sobre la Avenida Usme # 96 a 97 sur. 
+                        Es un punto estratégico para el sistema hídrico de Bogotá; cuenta con amplias zonas verdes, espacios de recreación, senderos ecológicos, caminos de piedra, miradores, entre otros espacios.</font></p>
+
+                        <h2>Descripción del recorrido </h2>
+
+                        <p><font size="3">El recorrido da inicio en el ingreso principal, donde un intérprete del parque nos dará la bienvenida, se socializaran las recomendaciones y se hará un calentamiento previo.</font></p>
+
+                        <center><h3>“APTA PARA TODA LA FAMILIA” </h3></center>
+                        <center><h3>En esta ocasión los participantes podrán asistir con su mascota canina</h3></center>
+                        <h3>INDISPENSABLE LLEVAR</h3>
+                        <ul class="ul_inicio">
+                            <font size="3">
+                            <li>Collar</li>
+                            <li>Carné de vacunación</li>
+                            <li>Bolsa para desechos</li>
+                            <li>Favor acogerse a la reglamentación vigente para la tenencia de razas peligrosas.</li>
+                            </font>
+                        </ul>
+                        <br>
+
+                        <font size="4"><center><table width="1000">
+
+                            <col width="281">
+
+                                <col width="282">
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Punto de encuentro</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Estación de Policía de USME. Av Usme No. 96A-97</p>
+
+                                        </td>
+
+                                    </tr>
+                              
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Fecha</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Domingo 05 de Marzo de 2017</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Hora</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>8:00 a.m</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Cupo Máximo</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>30 Personsa</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Rango de Edad</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>12 - 60 Años</p>
+
+                                        </td>
+
+                                    </tr>
+
+
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Altitud</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>2.600 m.s.n.m</td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Temperatura</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>14°</p>
+
+                                        </td>
+
+                                    </tr>
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Acceso</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>SITP:
+                                                <li>802 Sabana del Dorado – Usme</li>
+                                                <li>910 La Rivera – Usme</li>
+                                                <li>914 Bachue – Usme</li>
+
+                                               ALIMENTADOR:
+                                                <li>3-5 Portal Usme -Usminia</li>
+                                                </p>
+                                        </td>
+
+                                    </tr>
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Duración</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>3 horas</p>
+
+                                        </td>
+
+                                    </tr>
+                                     
+
+                        </table></center></font>
+
+                        <h2>Recomendaciones</h2>
+
+                        <ul class="ul_inicio">
+
+                            <font size="3">
+
+                                <li>Llevar ropa cómoda</li>
+                            
+                                <li>Llevar bloqueador solar</li>
+
+                                <li>Hidratación</li>
+
+                                <li>Calzado cómodo</li>
+
+                                <li>Llevar carné de la eps</li>
+
+                                <li>Refrigerio.</li>
+
+                                <li>Medicamentos personales </li>
+
+                                <li>No llevar mascotas </li>
+
+                                <li>Abstenerse de participar: mujeres en estado de embarazo, personas que tengan problemas de salud complejos</li>
+
+                                <li><ins>Abstenerse de asistir si no se encuentra inscrito</ins></li>
+                            </font>
+
+                            <br>
+
+                        </ul> <a class="btn btn-info" href="registro">Registrarse</a> </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+</section>
+
+@stop
